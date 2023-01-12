3eb50001d33ccb1b832762f1c489c7c4c6dcb40c
Josey Sekine <ipho.7johero@icloud.com>
2023-01-12 10:05:28 -0400

    routes and params practice

--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,23 @@ const express = require('express');
 const path = require('path');
 // const rootDir = require('../util/path')
 
-const productController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 const products = []
 
-router.get('/products', productController.getAdminProducts);
+router.get('/products', adminController.getAdminProducts);
 
 // /admin/add-product => GET
-router.get('/add-product', productController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct );
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
