299e3b4b8d9361994b56a6b4fe4c160954255da6
Josey Sekine <ipho.7johero@icloud.com>
2023-02-04 16:19:30 -0800

    sequelize CRUD syntax

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
   const express = require('express');
   const bodyParser = require('body-parser');
   const path = require('path');
-  const db = require('./util/database');
+  const sequelize = require('./util/database');
+
+  //model
+  const Product = require('./models/product');
+  const User = require('./models/user');
+  const Cart = require('./models/cart');
+  const CartItem = require('./models/cart-item');
+  const OrderItem = require('./models/order-item');
+  const Order = require('./models/order');
 
   const app = express();
 
@@ -11,25 +19,62 @@
   const adminRoutes = require('./routes/admin');
   const shopRoutes = require('./routes/shop');
 
-
-  // db.execute('SELECT * FROM products')
-  // .then((result) => {
-  //   console.log(result[0]);
-  // })
-  // .catch(result => {
-  //   console.log(result);
-  // });
-
-  // controller
-  const error = require('./controllers/404');
-
   app.use(bodyParser.urlencoded({extended: false}));
   // app.use(express.static('public'))
   app.use(express.static(path.join(__dirname, 'public')))
+
+  app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+  })
+
+
+  // controller
+  const error = require('./controllers/404');
 
   app.use('/admin', adminRoutes);
   app.use(shopRoutes);
 
   app.use(error.pageNotFound);
 
-  app.listen(3000);
+  // syncs the table that you define in the database.js file into db
+
+  // cascade means delete dependency
+  // association
+  Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+  User.hasMany(Product);
+  User.hasOne(Cart);
+  Cart.belongsTo(User);
+  Cart.belongsToMany(Product, { through: CartItem });
+  Product.belongsToMany(Cart, { through: CartItem });
+  Order.belongsTo(User);
+  User.hasMany(Order);
+  Order.belongsToMany(Product, { through: OrderItem});
+
+
+  sequelize
+  // .sync({ force: true })
+  // .sync()
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log('-------------------------------------------------');
+    console.log(err);
+  });
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,8 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
+const User = require('../models/user');
 
 // admin
 exports.getAddProduct = (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
   res.render('admin/edit-product', {
     pageTitle: "Add product",
     path: "/admin/add-product",
@@ -16,13 +16,21 @@ exports.postAddProduct  = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-  .save()
-  .then(() => {
-    res.redirect('/');
+
+  req.user 
+  .createProduct({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
   })
-  .catch(err => console.log(err));
+  .then(result => {
+    console.log('Created Product');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 
 exports.getEditProduct = (req, res, next) => {
@@ -32,7 +40,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if(!product) {
       return res.redirect('/');
     }
@@ -43,6 +54,9 @@ exports.getEditProduct = (req, res, next) => {
       product: product
     });
   })
+  .catch(err => {
+    console.log(err);
+  })
 };
 
 
@@ -51,31 +65,46 @@ exports.postEditProduct = (req, res, next) => {
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
-  const updatedDec = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedPrice,
-    updatedDec
-    )
-    updatedProduct.save();
+  const updatedDesc = req.body.description;
+  Product.findByPk(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    product.save();
+  })
+  .then(result => {
+    console.log("Updated");
     res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err));
 }
 
 exports.getAdminProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  req.user.getProducts()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err => {
+    console.log(err);
+  })
 }
 
 
 exports.postDeleteProduct = (req, res, next) =>{
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log('Destoryed product');
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err))
 }
