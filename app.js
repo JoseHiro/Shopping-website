9034240a3d5b29cfe550d6da0f93fd39500c9d3e
Josey Sekine <ipho.7johero@icloud.com>
2023-02-17 22:46:12 -0800

    Cookies permanent cookie VS session cookie

--- app.js
+++ app.js
@@ -1,54 +1,76 @@
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const path = require('path');
-  const mongoose = require('mongoose');
-  const User = require('./models/user');
-  const app = express();
+const path = require('path');
 
-  // controller
-  const error = require('./controllers/404');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
-  app.set('view engine', 'ejs');
-  app.set('views', 'views');
+const error = require('./controllers/404');
+const User = require('./models/user');
 
-  const adminRoutes = require('./routes/admin');
-  const shopRoutes = require('./routes/shop');
+const MONGODB_URI =
+  'mongodb+srv://messi:goat@cluster0.qzoei47.mongodb.net/shop';
 
-  app.use(bodyParser.urlencoded({extended: false}));
-  // app.use(express.static('public'))
-  app.use(express.static(path.join(__dirname, 'public')))
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
-  app.use((req, res, next) => {
-    User.findById('63e85aa863041ebe5c20095a')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}));
+
+app.use((req, res, next) => {
+  if(!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
+  .then(user => {
+    req.user = user;
+    next();
   })
+  .catch(err => console.log(err));
+})
 
-  app.use('/admin', adminRoutes);
-  app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
 
-  app.use(error.pageNotFound);
+app.use(error.pageNotFound);
 
-  mongoose
-  .connect('mongodb+srv://JoseHiro:goal@cluster0.qzoei47.mongodb.net/shop?retryWrites=true&w=majority')
-  .then(result => {
-    User.findOne().then(user => {
-      if(!user) {
-        const user = new User({
-          name: "Josey",
-          email: "aaa@aaa",
-          cart: {
-            items: []
-          }
-        });
-      }
-      user.save();
-    });
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  })
+mongoose
+.connect(MONGODB_URI)
+.then(result => {
+  User.findOne().then(user => {
+    if(!user) {
+      const user = new User({
+        name: "Josey",
+        email: "aaa@aaa",
+        cart: {
+          items: []
+        }
+      });
+    }
+    user.save();
+  });
+  app.listen(3000);
+})
+.catch(err => {
+  console.log(err);
+})
--- controllers/404.js
+++ controllers/404.js
@@ -3,6 +3,7 @@ exports.pageNotFound = (req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
   res.status(404).render("404", {
     pageTitle: "Page not found",
-    path: null
+    path: null,
+    isAuthenticated: req.session.isLoggedIn
   })
 }
