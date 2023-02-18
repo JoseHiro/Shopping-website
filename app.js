156cb173fed23e51d4669abc86eafea0e22f0c50
Josey Sekine <108254240+JoseHiro@users.noreply.github.com>
2023-02-18 07:51:37 -0800

    Update app.js
    Omitting to use the URI in Github

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const error = require('./controllers/404');
 const User = require('./models/user');
 
-const MONGODB_URI =
-  'mongodb+srv://messi:goat@cluster0.qzoei47.mongodb.net/shop';
+const MONGODB_URI = '';
 
 const app = express();
 const store = new MongoDBStore({
