bc94d42561b9341bfec910c6aa2136b315330cb1
Josey Sekine <ipho.7johero@icloud.com>
2023-02-25 09:40:52 -0600

    Authentication & flash messages

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const error = require('./controllers/404');
 const User = require('./models/user');
 
-const MONGODB_URI = '';
+const MONGODB_URI =
+  'mongodb+srv://messi10:goal@cluster0.qzoei47.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
@@ -17,8 +20,7 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
-
-
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -36,6 +38,9 @@ app.use(session({
   store: store
 }));
 
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
   if(!req.session.user) {
     return next();
@@ -49,6 +54,12 @@ app.use((req, res, next) => {
   .catch(err => console.log(err));
 })
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -58,18 +69,6 @@ app.use(error.pageNotFound);
 mongoose
 .connect(MONGODB_URI)
 .then(result => {
-  User.findOne().then(user => {
-    if(!user) {
-      const user = new User({
-        name: "Josey",
-        email: "aaa@aaa",
-        cart: {
-          items: []
-        }
-      });
-    }
-    user.save();
-  });
   app.listen(3000);
 })
 .catch(err => {
