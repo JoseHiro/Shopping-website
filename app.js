4bc7771103233d1224e7b7e674e705c24e025aca
Josey Sekine <ipho.7johero@icloud.com>
2023-03-24 17:26:27 -0700

    done

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use((error, req, res, next)=> {
 })
 
 mongoose
-.connect(MONGODB_URI)
+.connect(MONGODB_URI,{ useNewUrlParser: true })
 .then(result => {
   // https.createServer({key: privateKey, cert: certificate}, app)
   // .listen(process.env.PORT || 3000);
